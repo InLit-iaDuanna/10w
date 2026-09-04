a5f29af65f446244a7fe951d034a135ef6a7f1af
InLit-iaDuanna <isduanna@gmail.com>
2026-09-05 01:35:56 +0800

    feat: add integration center observability

---
+++ sceneops_forge_codex_full_pack_v3/modules/integration-center/frontend/src/index.ts
@@ -0,0 +1,46 @@
+export type {
+  IntegrationCenterClient,
+  IntegrationQueryContext,
+  RecoveryCommandInput,
+} from "./client.ts";
+export {
+  cancelJobCommand,
+  integrationCenterCommands,
+  openLogsCommand,
+  openSetupCommand,
+  refreshHealthCommand,
+  restartGuidanceCommand,
+  resumeJobCommand,
+  retryJobCommand,
+} from "./commands.ts";
+export type {
+  CommandAvailability,
+  CommandContext,
+  CommandDefinition,
+  OpenEditorResult,
+} from "./commands.ts";
+export { createIntegrationHealthEditorView } from "./editors/IntegrationHealthEditor.ts";
+export { createWorkerMonitorEditorView } from "./editors/WorkerMonitorEditor.ts";
+export {
+  defaultHealthEditorState,
+  defaultWorkerEditorState,
+  integrationHealthEditor,
+  moduleContribution,
+  workerMonitorEditor,
+} from "./manifest.ts";
+export type {
+  EditorPhase,
+  ExecutionMode,
+  HealthSummaryState,
+  IntegrationHealthEditorState,
+  IntegrationHealthEditorView,
+  IntegrationHealthView,
+  JobState,
+  JobView,
+  LogSummaryView,
+  RecommendedActionView,
+  RecoveryResultView,
+  WorkerMonitorEditorState,
+  WorkerMonitorEditorView,
+  WorkerView,
+} from "./types.ts";
